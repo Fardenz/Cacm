5fe2f14e2f01ee0d33cde4df679f24020a487432
Fardenz <javibodi@gmail.com>
2020-02-13 09:18:53 +0100

    testing bruteforce solution

--- cryptKicker/cryptKicker.cpp
+++ cryptKicker/cryptKicker.cpp
@@ -1,12 +1,128 @@
 
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
 #include <vector>
+#include <map>
+
 #include <string>
 
+using namespace std;
+string controlString = "(NULL)";
+
+//from http://www.cplusplus.com/articles/2wA0RXSz/
+const vector<string> split(const string &s, const char &c)
+{
+    string buff{""};
+    vector<string> v;
+
+    for (auto n : s)
+    {
+        if (n != c)
+            buff += n;
+        else if (n == c && buff != "")
+        {
+            v.push_back(buff);
+            buff = "";
+        }
+    }
+    if (buff != "")
+        v.push_back(buff);
+
+    return v;
+}
+string transformToAsterisk(vector<string> words)
+{
+
+    string res = "";
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        string word = words[i];
+        for (int j = 0; j < word.length(); j++)
+        {
+            res += "*";
+        }
+        res += " ";
+    }
+    return res;
+}
+string canDecrypt(vector<string> words, map<char, char> dict)
+{
+    string res = "";
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        string word = words[i];
+        for (int j = 0; j < word.length(); j++)
+        {
+            if (dict[word.at(j)])
+            {
+                res += dict[word.at(j)];
+            }
+            else
+            {
+                return controlString;
+            }
+        }
+        res += " ";
+    }
+    return res;
+}
+
+void processCase(string frase, map<int, vector<string>> possibleWords)
+{
+    vector<string> words = split(frase, ' ');
+
+    map<char, char> dict;
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        string word = words[i];
+
+        for (int pw = 0; pw < possibleWords[word.length()].size();)
+        {
+            map<char, char> tempDict = dict;
+            bool isWrong = false;
+
+            string decWord = possibleWords[word.length()][pw];
+            for (int j = 0; j < word.length(); j++)
+            {
+                if (!tempDict[word.at(j)])
+                {
+                    tempDict[word.at(j)] = decWord.at(j);
+                }
+                else
+                {
+                    if (tempDict[word.at(j)] != decWord.at(j))
+                    {
+                        //worng dict
+                        isWrong = true;
+                        pw++;
+                    }
+                }
+            }
+
+            if (!isWrong)
+            {
+                //merge maps
+                dict.insert(tempDict.begin(), tempDict.end());
+            }
+        }
+
+        string res = canDecrypt(words, dict);
+        if (res == controlString)
+        {
+            //cant decrypt
+            res = transformToAsterisk(words);
+        }
+        cout << res << "\n";
+    }
+}
 int main(int argc, char const *argv[])
 {
-    char linia[80];
-    std::vector<std::string> words(1000);
+    char linia[100];
+    map<int, vector<string>> possibleWords;
+    string s;
 
     int c, numWords, i, n;
     int P[1000];
@@ -14,12 +130,20 @@ int main(int argc, char const *argv[])
     numWords = c;
     while (--c >= 0)
     {
-        fgets(linia, 80, stdin);
+        cin >> s;
 
-        scanf(" %d", &n);
-        for (i = 0; i < n; i++)
-            scanf(" %d", &P[i]);
-        /* Aqu´ı solucionar cada caso */
+        possibleWords[s.length()].push_back(s);
     }
+
+    //get stdin till next \n
+    getline(cin, s);
+
+    //solve cases
+    while (cin)
+    {
+        getline(cin, s);
+        processCase(s, possibleWords);
+    }
+
     return 0;
 }
