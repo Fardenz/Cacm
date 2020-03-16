c1b6584906fd1dd2b117f55223f1a20abfba2c63
Fardenz <javibodi@gmail.com>
2020-03-16 12:46:22 +0100

    .

--- pokerHands/poker.cpp
+++ pokerHands/poker.cpp
@@ -28,6 +28,72 @@ const vector<string> split(const string &s, const char &c)
 }
 bool straightFlush(std::map<char, int> sw, std::map<char, int> vw, std::map<char, int> sb, std::map<char, int> vb)
 {
+
+    bool canBew = false, canBeb = false;
+    bool isw = false, isb = false;
+
+    std::map<char, int>::iterator it = sw.begin();
+    // Iterate over the map using c++11 range based for loop
+    for (std::pair<char, int> element : sw)
+    {
+        // Accessing KEY from element
+        char key = element.first;
+        // Accessing VALUE from element.
+        int val = element.second;
+
+        if (val == 5)
+        {
+            canBew = true;
+        }
+    }
+
+    it = sb.begin();
+    for (std::pair<char, int> element : sb)
+    {
+        char key = element.first;
+        int val = element.second;
+
+        if (val == 5)
+        {
+            canBeb = true;
+        }
+    }
+
+    if (!canBew && !canBeb)
+        return -1;
+
+    //checkw
+
+    it = vw.begin();
+    int last = -1, first = -1;
+    for (std::pair<char, int> element : vw)
+    {
+        char key = element.first;
+        int val = element.second;
+
+        if (val != 1)
+            break;
+
+        if (last == -1)
+        {
+            first = key;
+            last = key;
+        }
+        else
+        {
+            if (key != last + 1)
+                break;
+            else
+                last++;
+        }
+    }
+    if (last - first == 5)
+    {
+        isw = true;
+    }
+
+    //if cant be b return
+    //checkb
 }
 /*bool fourOfKind(std::map<char, int> sw, std::map<char, int> vw, std::map<char, int> sb, std::map<char, int> vb)
 {
@@ -42,7 +108,7 @@ bool straightFlush(std::map<char, int> sw, std::map<char, int> vw, std::map<char
         }
 
     //black
-    for (map<char, int>::iterator it = vb.begin(); it != vb.end(); ++it)
+for (map<char, int>::iterator it = vb.begin(); it != vb.end(); +ºQA<    A   q   AAAAa   º   +it)
     {
     }
 
@@ -62,17 +128,18 @@ int getWinner(vector<string> white, vector<string> black)
 
     for (int i = 0; i < 5; i++)
     {
-        valueW[white[i].at(0)];
-        valueB[black[i].at(0)];
+        valueW[white[i].at(0)]++;
+        valueB[black[i].at(0)]++;
 
-        suitW[white[i].at(1)];
-        suitB[black[i].at(1)];
+        suitW[white[i].at(1)]++;
+        suitB[black[i].at(1)]++;
     }
 
-    /*  int res = straightFlush(suitW, valueW, suitB, valueW);
+    int res = straightFlush(suitW, valueW, suitB, valueW);
     if (res > -1)
     {
-    } */
+        return res;
+    }
 }
 
 void processCase(vector<string> game)
